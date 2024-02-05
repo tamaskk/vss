816936ab134ec7c8c395a3a979e983a26e65a1c5
Boldizsar Fodor <fodorboldi7@gmail.com>
2024-02-05 15:42:54 +0100

    Dir update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+  },
+};
 
 export default nextConfig;
